780f0a3c1d5bce911c932fa8da702a34436061cf
Anderson de Souza <anderson17ads@hotmail.com>
2025-01-15 01:57:12 -0300

    Implement some exception in the handle

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,18 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\JsonResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    protected $dontReport = [
+        \Illuminate\Validation\ValidationException::class,
+    ];
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +33,32 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // ValidationException
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $exception->errors(),
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // Exception
+        if ($exception instanceof Exception) {
+            return response()->json([
+                'message' => 'An internal error has occurred',
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // ApiException
+        if ($exception instanceof ApiException) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        return parent::render($request, $exception);
     }
 }
